db3e24b4b25b182bf84b808e97a217e1f8be8649
HuuSang <huusang2k4@gmail.com>
2024-09-26 09:48:00 +0700

    Version 1.2 Update: Confirm Password in Register

--- KoiCareSystemAtHome/KoiCareSystemAtHome/Models/RegisterDTO.cs
+++ KoiCareSystemAtHome/KoiCareSystemAtHome/Models/RegisterDTO.cs
@@ -5,7 +5,8 @@
         public string Email { get; set; } = null!; 
         public string FullName { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
-        public string Password { get; set; } = null!;   
+        public string Password { get; set; } = null!;
+        public string Confirm_Password { get; set; } = null!;
 
     }
 
