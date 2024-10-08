5ecdf12bced64bd29d5959491dd1e8aa07a3b835
soc day <socpro13579@gmail.com>
2024-10-08 21:31:41 +0700

    Delete userController (duplicate with usercontrollers) Update ordercontroller (update status payment) Add VNPay (In progress)

---
+++ KoiCareSystemAtHome/KoiCareSystemAtHome/Repositories/IRepositories/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿//using KoiCareSystemAtHome.Models;
+
+//namespace KoiCareSystemAtHome.Repositories.IRepositories
+//{
+//    public class IVnPayService
+//    {
+//        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+//        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+//    }
+//}
